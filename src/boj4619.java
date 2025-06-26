eefbb99851f41d0225b15a3ef08b76d3e5c6e26c
bk-git-hub <bkprivate99@gmail.com>
2025-06-26 13:44:20 +0900

    boj4619

---
+++ src/boj4619.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class boj4619 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while(true){
+
+            String line = br.readLine();
+            if(line == null || line.equals("0 0")){
+                break;
+            }
+            StringTokenizer st = new StringTokenizer(line);
+
+            int B = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+
+            int a1 = (int) Math.pow(B, 1.0 / N);
+            int a2 = a1 + 1;
+
+            long diff1 = Math.abs(B - (long) Math.pow(a1, N));
+            long diff2 = Math.abs(B - (long) Math.pow(a2, N));
+
+            int result = (diff1 <= diff2) ? a1 : a2;
+            sb.append(result).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}

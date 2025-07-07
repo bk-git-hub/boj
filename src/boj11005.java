477dfe37bb5017e4f107ec39354728f0bd2c944d
bk-git-hub <bkprivate99@gmail.com>
2025-07-07 11:52:11 +0900

    boj11005

---
+++ src/boj11005.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class boj11005 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+
+        while(N>0){
+            int remainder = N%B;
+            if (remainder >= 10) {
+                sb.append((char) ('A' + (remainder - 10)));
+            } else {
+                sb.append(remainder);
+            }
+            N/=B;
+        }
+
+        System.out.println(sb.reverse());
+    }
+}

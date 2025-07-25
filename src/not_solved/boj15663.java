6edb4388b8a4bece9f05b4243e14c222bbb5483f
bk-git-hub <bkprivate99@gmail.com>
2025-07-25 15:17:05 +0900

    꺼져

---
+++ src/not_solved/boj15663.java
@@ -0,0 +1,33 @@
+package not_solved;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class boj15663 {
+
+    private static StringBuilder sb;
+    private static int[] counter;
+
+    private static int[] arr;
+    private static boolean[] visited;
+    private static int N;
+    private static int M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+
+        System.out.print(sb);
+    }
+
+    public static void build( int depth){
+
+    }
+}

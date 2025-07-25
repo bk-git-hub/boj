474f5ec3706c2046460ceccacc7834e8b8c5926d
bk-git-hub <bkprivate99@gmail.com>
2025-07-25 16:08:58 +0900

    어렵다 어려워

---
+++ src/not_solved/boj10972.java
@@ -0,0 +1,16 @@
+package not_solved;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class boj10972 {
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+
+    }
+}
